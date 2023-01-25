b70e62a661a3253e7f532af6fbc70b2d103be832
limbxbx <ilya.msd2003@gmail.com>
2023-01-25 03:36:20 +0200

    task 2 and 3 completed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        double c = (Math.pow(9*a,2)-5*b+a-7)*((a+b-4*a*b)/(2.0));
     }
 }
